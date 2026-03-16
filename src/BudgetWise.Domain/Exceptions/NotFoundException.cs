8308a72d9da389c6e98174c5f1597141d2f9e971
Victor Moura <victormoura@proton.me>
2026-03-16 16:58:53 -0300

    feat(domain): add domain exceptions (DomainException, NotFoundException, ForbiddenException)

---
+++ src/BudgetWise.Domain/Exceptions/NotFoundException.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BudgetWise.Domain.Exceptions;
+
+public class NotFoundException(string message) : Exception(message);
