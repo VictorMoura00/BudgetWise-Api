11031385eae5fcbdbe7077783423bd5a09599704
Victor Moura <victormoura@proton.me>
2026-03-16 16:21:22 -0300

    feat(api): add SerilogExtensions with Seq sink

---
+++ src/BudgetWise.Api/Extensions/SerilogExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Serilog;
+using Serilog.Formatting.Json;
+using Serilog.Enrichers.Environment;
+namespace BudgetWise.Api.Extensions;
+
+public static class SerilogExtensions
+{
+    public static void AddSerilogConfiguration(this WebApplicationBuilder builder)
+    {
+        builder.Logging.ClearProviders();
+
+        var loggerConfig = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder.Configuration)
+            .Enrich.FromLogContext()
+            .Enrich.WithMachineName()
+            .Enrich.WithEnvironmentName();
+
+        if (builder.Environment.IsDevelopment())
+        {
+            // Dev: logs legíveis no console
+            loggerConfig.WriteTo.Console();
+        }
+        else
+        {
+            // Produção: logs estruturados em JSON no console (para coleta por agentes externos)
+            loggerConfig.WriteTo.Console(new JsonFormatter());
+        }
+
+        // Seq: sempre ativo quando a URL estiver configurada
+        var seqUrl = builder.Configuration["Seq:ServerUrl"];
+
+        if (!string.IsNullOrEmpty(seqUrl))
+        {
+            loggerConfig.WriteTo.Seq(seqUrl);
+        }
+
+        var logger = loggerConfig.CreateLogger();
+
+        builder.Logging.AddSerilog(logger);
+        builder.Host.UseSerilog(logger);
+    }
+}
