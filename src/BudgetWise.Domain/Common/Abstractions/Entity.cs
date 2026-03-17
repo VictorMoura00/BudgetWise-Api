c20586da99adc71beb96f255e8c93e1943071ab1
Victor Moura <victormoura@proton.me>
2026-03-17 11:36:09 -0300

    feat(domain): add base abstractions (Entity, IAggregateRoot, ValueObject)

---
+++ src/BudgetWise.Domain/Common/Abstractions/Entity.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace BudgetWise.Domain.Common.Abstractions;
+
+public abstract class Entity : IEquatable<Entity>
+{
+    public Guid Id { get; protected set; } = Guid.NewGuid();
+    public DateTimeOffset CreatedAt { get; protected set; } = DateTimeOffset.UtcNow;
+    public DateTimeOffset? UpdatedAt { get; protected set; }
+    public bool IsDeleted { get; protected set; } = false;
+
+    protected void SetUpdated()
+    {
+        UpdatedAt = DateTimeOffset.UtcNow;
+    }
+
+    public void Delete()
+    {
+        IsDeleted = true;
+        SetUpdated();
+    }
+
+    public bool Equals(Entity? other)
+    {
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Id == other.Id;
+    }
+
+    public override bool Equals(object? obj) => Equals(obj as Entity);
+
+    public override int GetHashCode() => Id.GetHashCode();
+}
---
+++ src/BudgetWise.Domain/Common/Abstractions/ValueObject.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BudgetWise.Domain.Common.Abstractions;
+
+public abstract class ValueObject : IEquatable<ValueObject>
+{
+    protected abstract IEnumerable<object?> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null || obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        var other = (ValueObject)obj;
+        return GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    }
+
+    public bool Equals(ValueObject? other)
+    {
+        if (other is null || other.GetType() != GetType())
+            return false;
+
+        return GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    }
+
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+
+        foreach (var component in GetEqualityComponents())
+        {
+            hashCode.Add(component);
+        }
+
+        return hashCode.ToHashCode();
+    }
+
+    public static bool operator ==(ValueObject? left, ValueObject? right)
+    {
+        if (left is null && right is null)
+            return true;
+
+        if (left is null || right is null)
+            return false;
+
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(ValueObject? left, ValueObject? right)
+    {
+        return !(left == right);
+    }
+}
